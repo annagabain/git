0ddf3ff3754a32e6b666cbb8b580b02815a3fbb9
Anna Gabain <anna.gabain@outlook.com>
2022-05-10 12:09:05 +0200

    Create commands.sh

---
+++ commands.sh
@@ -0,0 +1,7 @@
+#Initializing git in a different editor
+#If you use a different editor than Gitpod, you will likely have to initialize git yourself. To do this, run the following command from your project directory:
+
+git init
+
+#Shows the status of our local repository
+git status
